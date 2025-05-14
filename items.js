f37d0a93507442ab2a8c8c48dd6b333a8f96dde8
MoonHongMyeong <moonhongmyeong@gmail.com>
2025-05-14 17:57:16 +0900

    script 파일 module type으로 분리

---
+++ items.js
@@ -0,0 +1,13 @@
+const items = [
+    {
+        id: 0, name: '바인드큐브', cash: 1000
+    },
+    {
+        id: 1, name: '촉매제', cash: 2500, limit: 10
+    },
+    {
+        id: 2, name: '흑룡 아티팩트', cash: 33000
+    }
+]
+
+export default items;
--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const items = [
-    {
-        id: 1, name: '바인드큐브', cash: '1000'
-    },
-    {
-        id: 2, name: '촉매제', cash: '2500'
-    },
-    {
-        id: 3, name: '흑룡 아티팩트', cash: '33000'
-    }
-]
+import items from './items.js';
 
 const main = document.querySelector('#main');
 
@@ -89,46 +79,35 @@ const updateResult = (id, auctionPrice) => {
         return;
     }
     const { minus5, current, plus5 } = calculate(price, cash);
+    // 데이터셋에 넣기
+    items[id].perMinus5 = minus5;
+    items[id].perCurrent = current;
+    items[id].perPlus5 = plus5; 
     document.querySelector(`#minus5-${id}`).textContent = `1 : ${(minus5).toFixed(2)}`;
     document.querySelector(`#current-${id}`).textContent = `1 : ${(current).toFixed(2)}`;
     document.querySelector(`#plus5-${id}`).textContent = `1 : ${(plus5).toFixed(2)}`;
 }
 
-const updateSellingItem = () => {
-    const teraPrice = document.querySelector('#tera-price').value;
-    const sellingItems = [];
-       
-    document.querySelectorAll('.current').forEach(item => {
-        const id = item.id.split('-')[1];
-        const cash = Number(item.textContent.split(':')[1]);
-        sellingItems.push({
-            id,
-            cash
-        });
-    })
+const calculateCashToTera = (e) => {
+    const targetTera = e.target.value;
+    const result = {};
+    const sortedItems = items.sort(function(a, b){
+        return b.perCurrent - a.perCurrent;
+    });
 
-    sellingItems.sort((a, b) => b.cash - a.cash);
-
-    const targetItem = sellingItems[0];
-    const targetItemName = document.querySelector(`#selling-item-name`);
-    targetItemName.textContent = items.find(item => item.id === Number(targetItem.id)).name;
-
-    const averagePrice = document.querySelector(`#average-price`);
-    const averagePer = document.querySelector(`#current-${targetItem.id}`).textContent.split(':')[1];
-    console.log(teraPrice, averagePer, items.find(item => item.id === Number(targetItem.id)).cash);
-    averagePrice.textContent = `${(teraPrice / averagePer).toFixed(2)}원`;
-
-    const minus5Price = document.querySelector(`#minus5-price`);
-    const minus5Per = document.querySelector(`#minus5-${targetItem.id}`).textContent.split(':')[1];
-    minus5Price.textContent = `${(teraPrice / minus5Per).toFixed(2)}원`;
     
-    const plus5Price = document.querySelector(`#plus5-price`);
-    const plus5Per = document.querySelector(`#plus5-${targetItem.id}`).textContent.split(':')[1];
-    plus5Price.textContent = `${(teraPrice / plus5Per).toFixed(2)}원`;
+
+    updateResultForTargetTera(result);
 }
+
+const updateResultForTargetTera = (calculateResult) => {
+    document.querySelector(``);
+}
+
 
 const init = () => {
     render(main, items);
+    document.querySelector('#target-tera').addEventListener('input', calculateCashToTera);
 }
 
 init();
