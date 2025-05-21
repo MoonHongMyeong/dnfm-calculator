597042ad9cce0a7fa05c56beb760c4879f44eb50
MoonHongMyeong <moonhongmyeong@gmail.com>
2025-05-21 14:19:48 +0900

    items 일관성을 가지게 촉매제 이외에도 limit property 적용.

--- items.js
+++ items.js
@@ -1,12 +1,12 @@
 const items = [
     {
-        id: 0, name: '바인드큐브', cash: 1000
+        id: 0, name: '바인드큐브', cash: 1000, limit: Infinity
     },
     {
         id: 1, name: '촉매제', cash: 2500, limit: 10
     },
     {
-        id: 2, name: '흑룡 아티팩트', cash: 33000
+        id: 2, name: '흑룡 아티팩트', cash: 33000, limit: Infinity
     }
 ]
 
