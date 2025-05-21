5f36f568db7d7dc7b6b57521f074beff1d45510c
MoonHongMyeong <moonhongmyeong@gmail.com>
2025-05-21 14:20:42 +0900

    그리디를 응용하여 목표 테라에 근접한 최적해를 도출해내는 함수 생성성

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const MARKET_FEE = 0.1;
 const TERA_TICK = 0.05;
 
 
-const render = (target, items) => {
+const initRender = (target, items) => {
     items.forEach(item => {
         const row = document.createElement('div');
         row.classList.add('flex-row');
@@ -29,7 +29,7 @@ const render = (target, items) => {
         teraInput.type = 'number';
         teraInput.id = `tera-${item.id}`;
         teraInput.oninput = (e) => {
-            updateResult(item.id, e.target.value);
+            displayTeraPerWon(item.id, e.target.value);
         }
 
         tera.appendChild(teraInput);
@@ -57,7 +57,7 @@ const render = (target, items) => {
     });    
 }
 
-const calculate = (auctionPrice, cash) => {
+const calculateTeraPerWon = (auctionPrice, cash) => {
     const result = auctionPrice * (1 - MARKET_FEE) / cash;
     return {
         minus5: result * (1 - TERA_TICK),
@@ -66,7 +66,7 @@ const calculate = (auctionPrice, cash) => {
     };
 }
 
-const updateResult = (id, auctionPrice) => {
+const displayTeraPerWon = (id, auctionPrice) => {
     const item = items.find(item => item.id === Number(id));
     if (!item) return;
     const cash = Number(item.cash);
@@ -78,36 +78,109 @@ const updateResult = (id, auctionPrice) => {
         document.querySelector(`#plus5-${id}`).textContent = '';
         return;
     }
-    const { minus5, current, plus5 } = calculate(price, cash);
+    const { minus5, current, plus5 } = calculateTeraPerWon(price, cash);
     // 데이터셋에 넣기
     items[id].perMinus5 = minus5;
     items[id].perCurrent = current;
-    items[id].perPlus5 = plus5; 
+    items[id].perPlus5 = plus5;
+    items[id].receiveTera = Math.ceil(auctionPrice * (1 - MARKET_FEE));// ceil(올림)일지 round(반올림)일지 모르겠음.
+
     document.querySelector(`#minus5-${id}`).textContent = `1 : ${(minus5).toFixed(2)}`;
     document.querySelector(`#current-${id}`).textContent = `1 : ${(current).toFixed(2)}`;
     document.querySelector(`#plus5-${id}`).textContent = `1 : ${(plus5).toFixed(2)}`;
 }
 
-const calculateCashToTera = (e) => {
-    const targetTera = e.target.value;
-    const result = {};
-    const sortedItems = items.sort(function(a, b){
+
+const calculateCashNeededForTera = (e) => {
+    let targetTera = Number(e.target.value);
+    const result = [];
+    const sortedItems = items.sort((a, b) => {
         return b.perCurrent - a.perCurrent;
     });
 
+    let totalTera = 0;
+    const minimumReceiveTera = Math.min(...items.map((x) => x.receiveTera));
+
+    // 어떤 아이템을 구매해도 의미없음.
+    if ( targetTera < minimumReceiveTera) {
+        console.log(targetTera, sortedItems[sortedItems.length -1].receiveTera)
+        document.querySelector('#average-item-list').textContent = ``;
+        return;
+    }
+
+    // greedy 방식으로 targetTera의 이하 근사값을 구한다.
+    for (const product of sortedItems) {
+        if (totalTera >= targetTera) break;
+
+        const remainingTera = targetTera - totalTera;
+        const maxCount = Math.floor(remainingTera / product.receiveTera);
+
+        if (maxCount > 0) {
+            product['count'] = maxCount;
+            result.push(product);
+            totalTera += maxCount * product.receiveTera;
+        }
+    }
     
+    // 추가 보정
+    if (totalTera < targetTera) {
+        const remainingTera = targetTera - totalTera;
+        let bestCandidate; //최적 후보
 
-    updateResultForTargetTera(result);
+        for (const product of sortedItems) {
+            const count = Math.ceil(remainingTera / product.receiveTera);
+            const addedTera = count * product.receiveTera;
+            const overAmount = addedTera - remainingTera;
+            const purchaseCost = count * product.cash;
+
+            if (!bestCandidate 
+                || overAmount < bestCandidate.overAmount 
+                || (overAmount === bestCandidate.overAmount 
+                    && purchaseCost < bestCandidate.purchaseCost)
+                ) {
+                bestCandidate = {
+                    name: product.name,
+                    count,
+                    addedTera,
+                    overAmount,
+                    purchaseCost
+                }
+            }
+        }
+
+        if (bestCandidate) {
+            const bestCandidateItem = result.find((element) => element.name == bestCandidate.name);
+            bestCandidateItem.count += bestCandidate.count;
+            totalTera += bestCandidate.addedTera;
+        }
+    }
+
+    displayCashNeededForTera(result);
 }
 
-const updateResultForTargetTera = (calculateResult) => {
-    document.querySelector(``);
-}
+const displayCashNeededForTera = (calculateResult) => {
+    calculateResult.sort((a, b) => (b.receiveTera * b.count) - (a.receiveTera * a.count));
 
+    const list = document.querySelector(`#average-item-list`);
+    let html = ``;
+    let totalTera = 0;
+    let totalCost = 0;
+
+    for ( let i=0; i < calculateResult.length; i++ ) {
+        html += `<p>${calculateResult[i].name} : ${calculateResult[i].count} (${calculateResult[i].receiveTera * calculateResult[i].count})</p>`;
+        totalTera += calculateResult[i].receiveTera * calculateResult[i].count;
+        totalCost += calculateResult[i].cash * calculateResult[i].count;
+    }
+
+    html += `<p>${totalTera} 테라</p>`;
+    html += `<p>${totalCost} 원</p>`
+
+    list.innerHTML = html;
+}
 
 const init = () => {
-    render(main, items);
-    document.querySelector('#target-tera').addEventListener('input', calculateCashToTera);
+    initRender(main, items);
+    document.querySelector('#target-tera').addEventListener('input', calculateCashNeededForTera);
 }
 
 init();
